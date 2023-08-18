d1fc62896469c91bdeef190958a8a5c29fe34179
Susant Kumar <susant@gmail.com>
2023-08-18 14:26:49 +0530

    TextFiled and State Example

--- app/src/main/java/com/zerocoder/MainActivity.kt
+++ app/src/main/java/com/zerocoder/MainActivity.kt
@@ -1,31 +1,56 @@
 package com.zerocoder
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+
 import androidx.compose.ui.tooling.preview.Preview
 import com.zerocoder.ui.theme.Compose_Test_AppTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             Greeting("Susant")
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Greeting(name: String) {
-    Text(
-        text = "Hello $name!",
-        )
+//    Text(
+//        text = "Hello $name!",
+//        textAlign = TextAlign.Center,
+//        color = Color.Blue,
+//        fontSize = 25.sp,
+//        fontStyle = FontStyle.Italic
+//    )
+//    Image(painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = "Call"
+//    )
+//    Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(
+//        containerColor= Color.Blue, contentColor = Color.Cyan
+//    )) {
+//        Text(text = "Submit")
+//    }
+    val state = remember { mutableStateOf("") }
+    TextField(
+        value = state.value,
+        onValueChange = { state.value = it },
+        label = { Text("Enter Your Message") })
+
 }
 
 @Preview(showBackground = true, showSystemUi = true)
